750ccf5d3416e0441dbd83b3a155498ab18d250a
ka1ea6 <kaleab77gulilat@gmail.com>
2022-03-09 23:55:56 -0800

    afsd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fprintf(stdout,"with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
