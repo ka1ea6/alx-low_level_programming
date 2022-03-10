76aa563a7f987bd10a7cd104c130c6628533f5cb
ka1ea6 <kaleab77gulilat@gmail.com>
2022-03-09 22:53:51 -0800

    corrections on 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void){
+int main(void)
+/**
+ * main function entry point 
+ * return value always a success (0)
+ */
+
+
+{
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
