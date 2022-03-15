5453cb1a4c277fe7f0686525816ed24e9ea99f47
ka1ea6 <kaleab77gulilat@gmail.com>
2022-03-15 02:22:43 -0700

    fout and two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - Prints the alphabet in lowercase 10 times
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 97; j < 122; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
