0b9a3d3041b12db3edb1373553144478b95b8998
ka1ea6 <kaleab77gulilat@gmail.com>
2022-03-15 00:59:47 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - calls print_alphabet() to print the alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+int _putchar(char);
+void print_alphabet(void);
+
+#endif
