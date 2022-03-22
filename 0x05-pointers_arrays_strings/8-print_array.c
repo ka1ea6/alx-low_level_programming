a8ae5d490cd481c03526a7f4f4ab93f9e70d6209
ka1ea6 <kaleab77gulilat@gmail.com>
2022-03-21 22:21:43 -0700

    eight and nine

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints the array up to the specified index.
+ *
+ * @a: the array to be printed.
+ *@n: the number of elements to print.
+ * Return: no return.
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		printf("%d, ", *(a + count));
+		count++;
+	}
+	printf("\n");
+}
