61083dcfce381e0d190ba76bf54db50744e4b967
ka1ea6 <kaleab77gulilat@gmail.com>
2022-03-24 01:42:59 -0700

    seven

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char *p;
+
+	p = leet(str);
+	printf("%s", p);
+	printf("%s", str);
+	return (0);
+}
