8f9c0020edb0c0e6b80ae93a1a7cc64506b2ad05
ka1ea6 <kaleab77gulilat@gmail.com>
2022-07-22 18:36:04 +0000

    9

---
+++ 0x09-static_libraries/3-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * _abs - returns the absolute value of the input
+ *
+ * @n: input number to compute absolute value.
+ *
+ * return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return n;
+	}
+	else {
+		return n * -1;
+	}
+}
---
+++ 0x09-static_libraries/9-atoi.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ *_atoi - function
+ *
+ * @s - input argument.
+ * return - integer
+ */
+int _atoi(char *s)
+{
+	return 0;
+}
