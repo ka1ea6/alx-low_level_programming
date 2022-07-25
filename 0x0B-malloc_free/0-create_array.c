1ac9ab639ada19e9c84e3c410f8be0324382074e
ka1ea6 <kaleab77gulilat@gmail.com>
2022-07-25 07:04:29 -0700

    betty corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and
+ * initializes it with a specific char
  *
  * @size: size of array to be created
  * @c: character used to initialize array
  *
  * Return: pointer to the created array
  */
-
 char *create_array(unsigned int size, char c)
 {
 	char *malPointer;
 	unsigned int i = 0;
-	
+
 	malPointer = malloc(sizeof(c) * size);
 	while (i < size)
 	{
@@ -22,5 +22,5 @@ char *create_array(unsigned int size, char c)
 		i++;
 	}
 
-	return malPointer;
+	return (malPointer);
 }
