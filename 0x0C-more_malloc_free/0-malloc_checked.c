3f5657996f3191ed6255ed67093ba9362ae64d8b
ka1ea6 <kaleab77gulilat@gmail.com>
2022-07-28 10:55:33 +0300

    0-push

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - returns a pointer to the allocated space if the 
+ * passed argument is valid.
+ *
+ * @b: size of the allocated memory.
+ *
+ * Return: pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+	
+	pointer = malloc(b);
+	if (pointer == NULL)
+		exit(98);
+	return (pointer);
+}
