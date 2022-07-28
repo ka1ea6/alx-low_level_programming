935a03abd65434d81a23116d34341a34170282e4
ka1ea6 <kaleab77gulilat@gmail.com>
2022-07-28 12:31:28 +0300

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range: returns pointer to an int array that
+ * includs values from min to max in asending order.
+ *
+ * @min: minimum number
+ * @max: maximum nubmer
+ * Return: pointer to an array of integers if valid, 
+ * NULL otherwise.
+ */
+
+int *array_range(int min, int max)
+{
+	int *intArray;
+	int i, len = 0;
+
+	if (min > max)
+		return (NULL);
+	len = (max - min + 1);
+	intArray = malloc(sizeof(int) * len);
+	if (intArray == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		intArray[i] = min;
+		min++;
+	}
+	return (intArray);
+}
+
