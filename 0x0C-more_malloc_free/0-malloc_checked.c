0dd03bc654156bb62269dc4094705929b54def1f
ka1ea6 <kaleab77gulilat@gmail.com>
2022-07-28 23:40:01 -0700

    betty styling on 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,8 @@
 #include "main.h"
 
 /**
- * malloc_checked - returns a pointer to the allocated space if the 
+ * malloc_checked - returns a pointer to the allocated space if the
  * passed argument is valid.
- *
  * @b: size of the allocated memory.
  *
  * Return: pointer to the allocated memory.
@@ -13,7 +12,7 @@
 void *malloc_checked(unsigned int b)
 {
 	void *pointer;
-	
+
 	pointer = malloc(b);
 	if (pointer == NULL)
 		exit(98);
