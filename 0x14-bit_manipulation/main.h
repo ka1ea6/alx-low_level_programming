f27a231b54ac5f2eecd1bc5188d0b7e7b6c9fa3b
ka1ea6 <kaleab77gulilat@gmail.com>
2022-08-18 21:34:26 +0300

    0x14

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,12 +5,12 @@
 
 unsigned int binary_to_uint(const char *b);
 int power(int, int);
-int _length(unsigned long int n)
-void print_binary(unsigned long int n)
-int _putchar(char c)
-int get_bit(unsigned long int n, unsigned int index)
-int set_bit(unsigned long int *n, unsigned int index)
-int clear_bit(unsigned long int *n, unsigned int index)
-unsigned int flip_bits(unsigned long int n, unsigned long int n)
+int _length(unsigned long int n);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
