379111095b3c745533c040d284c116cef3b26981
ka1ea6 <kaleab77gulilat@gmail.com>
2022-08-19 19:05:39 +0300

    '1correct'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,5 @@
 #include "main.h"
-/**
- * _length - find the length of @n
- *
- * @n: value to find its length
- *
- * Return: length
-*/
-int _length(unsigned long int n)
-{
-	int length = 0;
 
-	while (n > 0)
-	{
-		length++;
-		n >>= 1; /*shift n to the right by 1*/
-	}
-
-	length--;
-
-	return (length);
-}
 
 /**
  * print_binary - a function that converts a decimal to binary
@@ -30,34 +10,16 @@ int _length(unsigned long int n)
 */
 void print_binary(unsigned long int n)
 {
-	int length;
-	unsigned long int mask = 1;
-
-	length = _length(n);
-
-	if (length > 0) /*create mask based on length of number*/
-		mask <<= length; /*shift mask to the left by length*/
-
-	while (mask > 0)
+	if (n == 0)
 	{
-		if (n & mask) /*if n & mask == 1 print 1*/
-			_putchar('1');
-		else /*else if n & mask == 0 print 0*/
-			_putchar('0');
-
-		mask >>= 1; /*shift mask to the right by 1*/
+		putchar('0');
+		return;
 	}
-}
-
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	else if (n == 1)
+	{
+		putchar('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
