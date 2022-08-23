edc43d0833f4aa9c25e41aedade0d0dd9d0473fd
ka1ea6 <kaleab77gulilat@gmail.com>
2022-08-23 18:11:54 +0300

    correction on 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,6 +17,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+		text_content = "";
 	file = open(filename, O_APPEND | O_WRONLY);
 
 	if (!file)
