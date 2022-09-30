049cb99eb37a273b35e2ef9b5f4989cd0366e701
ka1ea6 <kaleab77gulilat@gmail.com>
2022-09-30 10:55:45 -0400

    7-up

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,10 +33,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	curr = *h;
 	while (curr)
 	{
-		if (current->next == NULL && cur_idx = idx - 1)
+		if (curr->next == NULL && cur_idx == idx - 1)
 		{
-			current->next = new;
-			new->prev = current;
+			curr->next = new;
+			new->prev = curr;
 			return (new);
 		}
 		else if (cur_idx == idx)
