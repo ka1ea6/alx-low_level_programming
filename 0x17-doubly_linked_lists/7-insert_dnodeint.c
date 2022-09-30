d4dc79bfa11ce2fee0fe1202f7fe0fd3747ba8d0
ka1ea6 <kaleab77gulilat@gmail.com>
2022-09-30 11:00:59 -0400

    7-up

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = NULL;
 	if (idx == 0)
 	{
-		*h = new;
+		new = add_dnodeint(h, n);
 		return (new);
 	}
 	cur_idx = 0;
@@ -49,7 +49,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		curr = curr->next;
 		cur_idx++;
 	}
-
+	free(new);
 	return (NULL);
 }
 
