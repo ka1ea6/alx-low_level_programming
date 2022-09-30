55308fd3c72a528b3011f55565108cd2c25a280f
ka1ea6 <kaleab77gulilat@gmail.com>
2022-09-30 12:37:25 -0400

    102

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -3,12 +3,10 @@ def largest_palindrome():
     largest = 0;
     for x in reversed(range(100, 1000)):
         for y in reversed(range(100, 1000)):
-            print(x, y, x * y)
             if is_palindrome(x * y):
-                largest = x * y
-                print(x, y)
-                return largest
-
+                if (x * y) > largest:
+                    largest = x * y
+    return largest
 def is_palindrome(num):
     str_num = str(num)
     for i in range(0, len(str_num)):
