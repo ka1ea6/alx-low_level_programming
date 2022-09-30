2c86b4e74c82ad4ba6361d11e636e63a6de69e69
ka1ea6 <kaleab77gulilat@gmail.com>
2022-09-30 11:08:17 -0400

    8-up

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -34,7 +34,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			temp->prev->next = temp->next;
 			temp->next->prev = temp->prev;
-			free(temp)
+			free(temp);
 			return (1);
 		}
 		temp = temp->next;
