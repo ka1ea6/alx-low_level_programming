06c0e006ec7abd9bb87db376308beb4a577ca7d3
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-04 03:54:06 -0400

    0x18 101

--- 0x18-dynamic_libraries/src/inject.c
+++ 0x18-dynamic_libraries/src/inject.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+/**
 static unsigned long next = 1;
 static unsigned int curr = 1;
 int rand(void)
@@ -23,4 +25,20 @@ int rand(void)
 void srand(unsigned int seed)
 {
 	next = 9;
+}
+*/
+int printf(__attribute__((unused))const char *format, ...)
+{
+	static int f = 0;
+	if (!f)
+		write(1, "9 8 10 24 75 - 9\n",17);
+
+	f = 1;
+	return (0);
+}
+
+int puts(__attribute__((unused))const char *s)
+{
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	return (0);
 }
