2809cb45bc5e1b135650bd906486ff47119975df
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-22 02:41:36 -0700

    2 bety

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,10 +3,10 @@
 /**
  * key_index - function to calculate the index a given key corresponds
  * to given the key and the size fo the hash table
- * 
+ *
  * @key: key to be indexed
  * @size: the size of the hash table for which the index is calcualted
- * 
+ *
  * Return: The index at which the key value pair should be stored
  */
 
