ebc8a6261a8c33b1e03424fea33e8f5953aec725
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-22 05:39:59 -0400

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function to calculate the index a given key corresponds
+ * to given the key and the size fo the hash table
+ * 
+ * @key: key to be indexed
+ * @size: the size of the hash table for which the index is calcualted
+ * 
+ * Return: The index at which the key value pair should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
