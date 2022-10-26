31590f7ea5acecf600b5860c93686ffdec46a956
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-26 11:56:03 -0400

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.c"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: pointer to the hash table
+ *
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
+
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
