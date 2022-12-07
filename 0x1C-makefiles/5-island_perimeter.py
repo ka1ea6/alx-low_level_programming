fb17d312f6c2a7918436b53e4f00089fe7645b25
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-07 18:18:25 +0300

    5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/dev python3
+'''Module defining a function for calculating a perimeter given\
+a grid '''
+
+
+def island_perimeter(grid):
+    '''Calculates the perimter of the given grid'''
+    perimter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perimter += (4 - check_neighbors(grid, row, col))
+    return perimter
+
+
+def check_neighbors(grid, row, col):
+    '''Returns the number of ones surrounding the given
+    row and col'''
+    neighbors = 0
+    if row > 0:
+        if grid[row - 1][col] == 1:
+            neighbors += 1
+    if row < len(grid) - 1:
+        if grid[row + 1][col] == 1:
+            neighbors += 1
+    if col > 0:
+        if grid[row][col - 1] == 1:
+            neighbors += 1
+    if col < len(grid[row]) - 1:
+        if grid[row][col + 1] == 1:
+            neighbors += 1
+    return neighbors
